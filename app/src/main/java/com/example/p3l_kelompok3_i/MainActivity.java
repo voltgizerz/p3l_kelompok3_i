02cdc93e48714e383908145604d4990372894e2d
voltgizerz <felix.asui@gmail.com>
2020-03-16 15:08:55 +0700

    UPDATE UI INFORMASI TOKO

--- app/src/main/java/com/example/p3l_kelompok3_i/MainActivity.java
+++ app/src/main/java/com/example/p3l_kelompok3_i/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCekLayanan;
     private Button btnCekProduk;
     private Button btnLogin;
+    private Button btnInformasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btnCekProduk = findViewById(R.id.btnCekProduk);
         btnCekLayanan = findViewById(R.id.btnCekLayanan);
         btnLogin = findViewById(R.id.btnLogin);
+        btnInformasi = findViewById(R.id.btnInfromasi);
 
 
         btnCekProduk.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+
+        btnInformasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Informasi.class);
                 startActivity(i);
             }
         });
