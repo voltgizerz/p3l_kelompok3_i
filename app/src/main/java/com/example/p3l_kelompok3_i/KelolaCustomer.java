61980834c7cf59f08a04a41be8b72bb957e64512
voltgizerz <felix.asui@gmail.com>
2020-03-16 19:56:27 +0700

    SUKSES CREATE CUSTOMER DAN JENIS HEWAN! FIX UI!

--- app/src/main/java/com/example/p3l_kelompok3_i/KelolaCustomer.java
+++ app/src/main/java/com/example/p3l_kelompok3_i/KelolaCustomer.java
@@ -2,56 +2,71 @@ package com.example.p3l_kelompok3_i;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.p3l_kelompok3_i.api.ApiClient;
+import com.example.p3l_kelompok3_i.api.ApiInterface;
+import com.example.p3l_kelompok3_i.model_customer.ResponCustomer;
+
+import java.util.Date;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class KelolaCustomer extends AppCompatActivity {
-
-    private Button btnMasukTambahCustomer;
-    private Button btnMasukTampilCustomer;
+    EditText nama_customer, alamat_customer,  nomor_hp_customer, tanggal_lahir_customer;
+    Button btncreate;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kelola_customer);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nama_customer = (EditText) findViewById(R.id.nama_customer);
+        alamat_customer = (EditText) findViewById(R.id.alamat_customer);
+        tanggal_lahir_customer = (EditText) findViewById(R.id.tanggal_lahir_customer);
+        nomor_hp_customer = (EditText) findViewById(R.id.nomor_hp_customer);
+        btncreate = (Button) findViewById(R.id.btn_create_customer);
+        pd = new ProgressDialog(this);
 
-        btnMasukTambahCustomer = findViewById(R.id.btnTambahCustomer);
-        btnMasukTampilCustomer = findViewById(R.id.btnTampilCustomer);
-
-        btnMasukTambahCustomer.setOnClickListener(new View.OnClickListener() {
+        btncreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(KelolaCustomer.this, TambahCustomer.class);
-                startActivity(i);
-            }
-        });
+                pd.setMessage("creating data..");
+                pd.setCancelable(false);
+                pd.show();
+                String snama = nama_customer.getText().toString();
+                String salamat = alamat_customer.getText().toString();
+                String stanggal_lahir = tanggal_lahir_customer.getText().toString();
+                String sno_hp = nomor_hp_customer.getText().toString();
 
-        btnMasukTampilCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KelolaCustomer.this, TampilCustomer.class);
-                startActivity(i);
+                ApiInterface api = ApiClient.getClient().create(ApiInterface.class);
+
+                Call<ResponCustomer> sendcustomer = api.sendCustomer(snama,salamat,stanggal_lahir,sno_hp);
+                sendcustomer.enqueue(new Callback<ResponCustomer>() {
+                    @Override
+                    public void onResponse(Call<ResponCustomer> call, Response<ResponCustomer> response) {
+                        pd.hide();
+                        Log.d("RETRO", "response: " + response.body().toString());
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponCustomer> call, Throwable t) {
+                        pd.hide();
+                        Log.d("RETRO", "Failure: " + "Gagal Mendaftar");
+
+                    }
+                });
+
             }
         });
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent intent = new Intent(this, MenuAdmin.class);
-                startActivity(intent);
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
 }
-
--- app/src/main/java/com/example/p3l_kelompok3_i/api/ApiInterface.java
+++ app/src/main/java/com/example/p3l_kelompok3_i/api/ApiInterface.java
@@ -1,5 +1,6 @@
 package com.example.p3l_kelompok3_i.api;
 
+import com.example.p3l_kelompok3_i.model_customer.ResponCustomer;
 import com.example.p3l_kelompok3_i.model_jasa_layanan.ResponLayanan;
 import com.example.p3l_kelompok3_i.model_jenis_hewan.ResponJenisHewan;
 import com.example.p3l_kelompok3_i.model_produk.ResponProduk;
@@ -22,6 +23,12 @@ public interface ApiInterface {
   @POST("jenis_hewan/create")
   Call<ResponJenisHewan> sendJenisHewan(@Field("nama_jenis_hewan") String nama_jenis_hewan);
 
+  @FormUrlEncoded
+  @POST("customer/create")
+  Call<ResponCustomer> sendCustomer(@Field("nama_customer") String nama_customer,
+                                    @Field("alamat_customer") String alamat_customer,
+                                    @Field("tanggal_lahir_customer") String tanggal_lahir_customer,
+                                    @Field("nomor_hp_customer") String nomor_hp_customer);
 
 
 
